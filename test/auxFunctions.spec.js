a15f41f32310937591a1b4868cbfa1b0c365a728
Angie Muñoz <aemunozpl@gmail.com>
2023-03-11 17:15:45 -0500

    test readFile

--- test/auxFunctions.spec.js
+++ test/auxFunctions.spec.js
@@ -1,20 +1,42 @@
+/* eslint-disable no-undef */
+
 const {
-    pathValid,
-    pathAbs,
-    onlyMd,
-    getLinks,
-    getLinkStatus,
-  } = require('../auxFunctions.js')
+	pathValid,
+	fileR,
+	pathAbs,
+	onlyMd,
+	getLinks,
+	getLinkStatus,
+} = require('../auxFunctions.js')
 
+describe('fs.existsSync,retora booleano para saber si la ruta existe o no', () => {
+	it('Deberia ser una función', () => {
+		expect(typeof pathValid).toBe('function');
+	});
+	it('Si la ruta existe, debe devolver true', () => {
+		expect(pathValid('prueba.md')).toBe(true);
+	});
+	it('Si la ruta no existe, debe devolver false', () => {
+		expect(pathValid('noexiste')).toBe(false);
+	});
+});
 
-  describe('existsSync, esta función retorna un booleano y verifica si existe un directorio o archivo', () => {
-    it('Deberia ser una función', () => {
-        expect(typeof existsSync).toBe('function');
-    });
-    it('Si la ruta existe, debe devolver true', () => {
-        expect(pathValid('prueba')).toBe(true);
-    });
-    it('Si la ruta no existe, debe devolver false', () => {
-        expect(pathValid('noexiste')).toBe(false);
-    });
+// Testeo de 
+
+describe('devuelve una promesa', () => {
+
+	test('the data is format link', () => {
+		// expect.assertions(1);
+		return fileR('prueba.md').then(data => {
+			expect(data).toBe(' [Funciones — bloques de código reutilizables - MDN](https://developer.mozilla.org/es/docs/Learn/JavaScript/Building_blocks/Functions)');
+		});
+	});
+});
+
+describe('devuelve una promesa', () => {
+	test('the fetch fails with an error', () => {
+		return fileR('prueb').catch(err =>
+			expect(err).toBe('error: ')
+		);
+	});
 });
