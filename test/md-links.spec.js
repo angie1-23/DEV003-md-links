b4ccfc09f998963b8a756c6f68ecc6c7b2e0f64b
Angie Muñoz <aemunozpl@gmail.com>
2023-03-15 08:39:44 -0500

    Tests

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,29 +1,36 @@
+/* eslint-disable no-undef */
 const { mdLinks }  = require('../index.js');
 
-
 describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
-  });
-
   it('Debe retornar un array con objetos con propiedades href, text, file', () => {
     const path = 'C:/Users/Estefania/Desktop/mdLinks/DEV003-md-links/prueba.md'
     const array = [{
-        href: 'https://developer.mozilla.org/es/docs/Learn/JavaScript/Building_blocks/Functions',
+        href: 'https://developer.mozilla.org/es/docs/Learn/JavaScript/Building_blocks/ Functions',
         text: 'Funciones — bloques de código reutilizables - MDN',
         file: 'C:/Users/Estefania/Desktop/mdLinks/DEV003-md-links/prueba.md',
     }];
     expect(mdLinks(path, { validate: false })).resolves.toEqual(array);//Se usa para probar que los objetos tienen los mismos tipos y estructura.
   });
 
-  it('Deberia rechazar cuando el path no existe', () => {
+  it('Debe retornar un array con objetos con propiedades href, text, file,status,message', () => {
+    const path = 'C:/Users/Estefania/Desktop/mdLinks/DEV003-md-links/prueba.md'
+    const array = [{
+        href: 'https://developer.mozilla.org/es/docs/Learn/JavaScript/Building_blocks/ Functions',
+        text: 'Funciones — bloques de código reutilizables - MDN',
+        file: 'C:/Users/Estefania/Desktop/mdLinks/DEV003-md-links/prueba.md',
+        status:404,
+        message:'Fail'
+    }];
+    expect(mdLinks(path)).resolves.toEqual(array);//Se usa para probar que los objetos tienen los mismos tipos y estructura.
+  });
+
+  it('Deberia rechazar si no encuentran links', () => {
     return mdLinks('C:/Users/Estefania/Desktop/mdLinks/DEV003-md-links/anyLinks.md').catch((error) => {
       expect(error).toBe('Any link found');
     })
   });
 
-  it('Deberia rechazar cuando el path no existe', () => {
+  it('Deberia rechazar cuando el archivo no sea Markdown', () => {
     return mdLinks('C:/Users/Estefania/Desktop/mdLinks/DEV003-md-links/pruebaTex.txt').catch((error) => {
       expect(error).toBe("Markdown (.md) files not found");
     })
