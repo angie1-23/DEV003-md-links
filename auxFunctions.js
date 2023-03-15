98cbecf2e2bc4c936034a09a6112e05167b32b84
Angie Muñoz <aemunozpl@gmail.com>
2023-03-15 08:32:38 -0500

    Function mdLinks concat

--- auxFunctions.js
+++ auxFunctions.js
@@ -10,18 +10,12 @@ const pathValid = (path) => { return fs.existsSync(path) };
 //Verificar si la ruta es absoluta o relativa y se convierte en absoluta
 const pathAbs = (way) => (path.isAbsolute(way) ? (way) : path.resolve(way));
 
+// Verificar si la ruta absoluta archivo 
 const isFile= (way) =>{return fs.statSync(way).isFile()};
-// console.log(isFile('C:/Users/Estefania/Desktop/mdLinks/DEV003-md-links/prueba.md'))
-// Verificar si la ruta absoluta es un directorio o archivo 
+
 const pathInfo = (way) => { return fs.statSync(way).isDirectory()};
-
-// Verificar si la ruta absoluta es un directorio o archivo 
-
-
 // Extension de un archivo 
 const fileMd = (way) => path.extname(way);
-
-// Filtrar la extension de un archivo
 
 // Leer un directorio 
 const readAllFiles = (way, arrayOfFiles = []) => {
@@ -41,18 +35,8 @@ const readAllFiles = (way, arrayOfFiles = []) => {
 	return arrayOfFiles
 };
 // console.log(readAllFiles('direc'));
+// console.log(readAllFiles('prueba.md'));
 const onlyMd = (way) => { return readAllFiles(way).filter((file => path.extname(file) === '.md')) };
-// console.log(onlyMd('prueba.md'));
-
-// console.log(onlyMd('direc'));
-
-
-// const returnOnlyFilesMd = (routeFile) => {
-//     return arrayForFiles(routeFile).filter((file => path.extname(file) === '.md'));
-// };
-
-// const chalk = require('chalk');
-// const figlet = require('figlet');
 
 // como leer un archivo
 const fileR = (way) => {
@@ -66,77 +50,6 @@ const fileR = (way) => {
 		});
 	});
 };
-// fileR('prueba.md').then((response) => {
-// 	console.log(response);
-// });
-
-// const getLinks = (linkmd) => {
-// 	return new Promise((resolve, reject) => {
-// 		let arrayLinks = [];
-// 			const rgexURL = /\(((?:\/|https?:\/\/).*)\)/g; // obtiene (links) contenidos en cada archivo
-// 			const regExp = /\[(.*)\]\(((?:\/|https?:\/\/).*)\)/gi; // obtiene [textoReferenciaDelLink] y (links)
-// 			const regText = /\[(.*)\]/g; // obtiene [textoReferenciaDelLink]
-// 			fileR(linkmd).then((response) => {
-// 				const other = response.match(regExp);
-// 				// let match = regEx.exec(response);
-// 				// console.log(other);
-// 				if (other != null) {
-// 					const arrayOfLinks = other.map((links) => {
-// 						const uniqueLinks = links.match(rgexURL).join().slice(1, -1);
-// 						const uniqueText = links.match(regText).join().slice(1, -1);
-// 						return {
-// 							href: uniqueLinks,
-// 							text: uniqueText,
-// 							file: linkmd,
-// 						};
-// 					});
-// 					arrayLinks = arrayLinks.concat(arrayOfLinks);
-// 				}
-// 				resolve(arrayLinks);
-// 				// console.log(arrayLinks);
-// 			})
-// 			.catch((error) => reject(error));
-// 	});
-// }
-// console.log(getLinks("direc"));
-
-// const getLinks = (way) => {
-// 	return new Promise ((resolve, reject)=> {
-// 	let arrayLinks =[];
-// 	onlyMd(way).forEach((linkmd, index, array)=>{
-// 		const regEx = /\[(.*)\]\(((?:\/|https?:\/\/).*)\)/gi;
-// 		// const arrayLinksWithText = readFile(res).match(regExp);
-// 		console.log("Index Fuera", index)
-// 		fileR(linkmd).then((response) => {
-//             // console.log(response);
-// 			// console.log(response.match(regEx));
-// 			let match;
-// 			// console.log(match)
-// 			console.log("Index dentro", index)
-// 			while((match = regEx.exec(response)) !== null) {
-// 				console.log(match)
-// 				console.log(linkmd, index)
-// 				arrayLinks.push({
-// 					href: match[2],
-//           text: match[1],
-// 					file: linkmd,
-// 				});
-// 				// match = regEx.exec(response);
-// 			}
-// 			// console.log(arrayLinks);
-// 			// console.log(arrayLinks)
-// 			if (index === (array.length -1)) {
-//         // This is the last one.
-// 				console.log(arrayLinks)
-//         resolve(arrayLinks);
-// 			}
-			
-// 		})
-// 		.catch((error) => reject('error: ',`${error}`));
-// 	});
-// });
-// };
-// getLinks('./direc').then((res)=>console.log(res)).catch((err) => console.log(err));
 
 const getLinks = (way) => {
 	return new Promise ((resolve, reject)=> {
@@ -144,29 +57,16 @@ const getLinks = (way) => {
 	const regEx = /\[(.*)\]\(((?:\/|https?:\/\/).*)\)/gi;
 	arrayLinks = onlyMd(way).map((linkmd) => fileR(linkmd)
 		.then((response) => {
-            // console.log(response);
-			// console.log(response.match(regEx));
 			let arrayAux = [];
 			let match;
-			// console.log(match)
-			// console.log("Index dentro", index)
 			while((match = regEx.exec(response)) !== null) {
-				// console.log(match)
-				// console.log(linkmd, index)
 				arrayAux.push({
 					href: match[2],
           text: match[1],
 					file: linkmd,
 				});
-				// match = regEx.exec(response);
 			}
 			return arrayAux;
-			// console.log(arrayLinks);
-			// console.log(arrayLinks)
-			// if (index === (array.length -1)) {
-      //   // This is the last one.
-			// 	console.log(arrayLinks)
-        // resolve(arrayLinks);
 			})
 
 		.catch((error) => reject('error: ',`${error}`))
@@ -177,10 +77,8 @@ const getLinks = (way) => {
 
 const getLinkStatus = (array) => {
 	let statusLink = []; 
-		// console.log(array);
 		statusLink = array.map((link) => fetch(link.href)
 			.then((response) => { 
-				// console.log(response);
 				return {
 				href: link.href,
 				text: link.text,
@@ -197,27 +95,17 @@ const getLinkStatus = (array) => {
 				message: error.code,
 			}
 			})
-		)
+		);
 	return Promise.all(statusLink);
 };
-		// console.log(getLinkStatus())
-		// getLinks('https://curriculum.laboratoria.la/es/topics/css/css/boxmodel-and-display').then(((res) => (getLinkStatus(res).then(((resolve) => console.log(resolve))))));
-		// getLinkStatus('direc').then((res)=> console.log(res)).catch((err)=>err)
-		// console.log(getLinkStatus('direc'));
-	// console.log(getLinkStatus('./direc'));
-// getLinkStatus('direc');
 
-
-// getLinks('direc').then(((res) =>(getLinkStatus(res).then(((resolve) => console.log(resolve))))));
-
-const concatenar = (arrayofarrays) => {
+const concatArr = (arrayofarrays) => {
 	let finalArray = [];
 	arrayofarrays.forEach(array => {
 		finalArray = finalArray.concat(array);
 	});
 	return finalArray
 };
-
 
 module.exports = {
 	pathValid,
@@ -230,5 +118,5 @@ module.exports = {
 	getLinks,
 	getLinkStatus,
 	isFile,
-	concatenar
+	concatArr,
 };
